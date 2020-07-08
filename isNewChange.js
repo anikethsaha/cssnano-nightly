f6f45095c3460c7f8b42f69587bf04dac9dede8a
Anix <anik220798@gmail.com>
2020-07-08 11:36:48 +0000

    build: changed new change check logic

--- isNewChange.js
+++ isNewChange.js
@@ -1,45 +1,26 @@
 const fetch = require("node-fetch");
-const { date } = require("./utils");
+const { date, getTodayDate, getYesterdayDate } = require("./utils");
+
+const { tdate, tmonth, tyear } = getTodayDate();
+const { ydate, ymonth, yyear } = getYesterdayDate();
+
+const yesterdayDate = `${yyear}-${ymonth}-${ydate}`;
+const todayDate = `${tyear}-${tmonth}-${tdate}`;
 
 let cssnanoLastCommitDate;
 
 async function isNewChange() {
-  let latestMasterCommit = await fetch(
-    "https://api.github.com/repos/cssnano/cssnano/commits/master",
+  let dm = await fetch(
+    //   temporary adding the "'2019-02-14'", replace it with `yesterdayDate`
+    `https://api.github.com/repos/cssnano/cssnano/commits?branch=master&since=${"2019-02-14"}&until=${todayDate}`,
     {
       headers: {
         Authorization: "token " + process.env.OAUTH_TOKEN
       }
     }
   );
-
-  latestMasterCommit = await latestMasterCommit.json();
-  console.log({ latestMasterCommit });
-  let cssnanoCommit = await fetch(
-    `https://api.github.com/repos/cssnano/cssnano/git/commits/${latestMasterCommit.sha}`,
-    {
-      headers: {
-        Authorization: "token " + process.env.OAUTH_TOKEN
-      }
-    }
-  );
-
-  cssnanoCommit = await cssnanoCommit.json();
-  cssnanoLastCommitDate = new Date(cssnanoCommit.committer.date);
-  let nightlyCommit = await fetch(
-    "https://api.github.com/repos/anikethsaha/cssnano-nightly/commits/master",
-    {
-      headers: {
-        Authorization: "token " + process.env.OAUTH_TOKEN
-      }
-    }
-  );
-
-  nightlyCommit = await nightlyCommit.json();
-  const { message, committer } = nightlyCommit.commit;
-  const lastNightlyPubCommit = new Date(committer.date);
-
-  return cssnanoLastCommitDate.getTime() > lastNightlyPubCommit.getTime();
+  dm = await dm.json();
+  return Array.isArray(dm) && dm.length > 0;
 }
 
 module.exports = isNewChange;
--- utils.js
+++ utils.js
@@ -8,5 +8,22 @@ const semver = {
 module.exports = {
   semver,
   version: `${semver.major}.${semver.minor}.${semver.patch}`,
-  date
+  date,
+  getTodayDate: () => {
+    const d = new Date();
+    return {
+      tdate: d.getUTCDate(),
+      tyear: d.getUTCFullYear(),
+      tmonth: d.getUTCMonth() + 1
+    };
+  },
+  getYesterdayDate: () => {
+    const d = new Date();
+    d.setDate(d.getDate() - 1);
+    return {
+      ydate: d.getUTCDate(),
+      yyear: d.getUTCFullYear(),
+      ymonth: d.getUTCMonth() + 1
+    };
+  }
 };
